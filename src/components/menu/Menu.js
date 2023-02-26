57bb47113bea511ab3878bf8e09b1b261b64b339
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-26 11:52:44 +0300

    REF: set routes

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../../style/Menu.css";
 import AuthenticationService from "../api/AuthenticationService";
 import { Link } from 'react-router-dom';
-
+import logo from '../../assets/find-out-my-loan.png'
 function Menu() {
   const [isLoggedOn, setIsLoggedOn] = useState(true);
  const handleLogout=()=>{
@@ -19,12 +19,20 @@ function Menu() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src="logo.svg" alt="logo" />
+        <img src={logo} alt="logo" />
         <h1>Find Out My Loan</h1>
       </div>
       <div className="menu-icon">&#9776;</div>
       <ul className="menu">
-
+      <li>
+        <Link to="/apply-loan">Loan Apply</Link>
+        </li>
+      <li>
+        <Link to="/find-loans">Find Loans</Link>
+        </li>
+      <li>
+        <Link to="/update-profile">Update Profile</Link>
+        </li>
         <li>
         <Link to="/login" onClick={handleLogout}>Logout</Link>
         </li>
